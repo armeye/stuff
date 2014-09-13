1f4ac4bb9c4dcda4b54a9fec4857414452aef32a
Arne Meyer <meyer.arne83@gmail.com>
2014-09-13 16:41:20 +0200

    fix disk img name

--- freebsd/bhyve/fbsd.sh
+++ freebsd/bhyve/fbsd.sh
@@ -1,9 +1,11 @@
-bhyveload -m 256M -d obsd.img fbsd
+#! /bin/s
+j
+bhyveload -m 256M -d fbsd.img fbsd
 
 bhyve -AI -H -P -W \
       -s 0:0,hostbridge \
       -s 1:0,lpc \
-      -s 2:0,virtio-blk,./obsd.img \
+      -s 2:0,virtio-blk,./fbsd.img \
       -s 4:0,virtio-rnd \
       -s 4:1,virtio-net,tap0 \
       -l com1,/dev/nmdm0A -m 256M fbsd
