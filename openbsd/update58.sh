d94597cecdf77148db2117832d4401a2b8777500
Arne Meyer <meyer.arne83@gmail.com>
2015-11-06 18:56:49 +0100

    update58.sh, script to delete leftovers from the old version

---
+++ openbsd/update58.sh
@@ -0,0 +1,6 @@
+#! /bin/sh
+
+rm -f /usr/bin/sudo /usr/bin/sudoedit /usr/sbin/visudo
+rm -f /usr/share/man/man8/sudo.8 /usr/share/man/man8/sudoedit.8
+rm -f /usr/share/man/man8/visudo.8 /usr/share/man/man5/sudoers.5
+rm -f /usr/libexec/sudo_noexec.so
